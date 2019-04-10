f3b6c83685e8158a40f1d5e503db631fc43c9b5e
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-10 17:31:45 -0400

    Add create action to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @donor = Donor.find_by(username: params[:donor][:username])
+        if @donor && @donor.authenticate(params[:donor][:password])
+            session[:donor_id] = @donor.id
+            redirect_to donor_path(@donor), notice: "Welcome back to the Virtual Blook Bank!"
+        else
+            redirect_to signin_path
+        end
 
     end
 
