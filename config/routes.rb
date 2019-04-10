32cb5b5ea75d36e9e2b101906d9c4686484be979
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-10 16:15:04 -0400

    Add authenticating and signout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  resources :donors
 end
