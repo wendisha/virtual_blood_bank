11a1aa74cd4141762ccc5ff905a5726ff925900a
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-10 22:31:30 -0400

    Add create action for donor

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -2,6 +2,17 @@ class DonorsController < ApplicationController
     def new
         @donor = Donor.new
     end
+
+    def create
+        @donor = Donor.new(donor_params)
+        if @donor.save
+          session[:donor_id] = @donor.id
+          flash[:message] = "Successfully Signed Up!"
+          redirect_to donors_path
+        else
+          render :new
+        end
+    end
  
     # whitelist the permitted params when sending form data to the db
     private
