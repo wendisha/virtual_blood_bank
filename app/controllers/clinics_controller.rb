4f02cdff039e9a9ade07374049af2a5048fac59c
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-12 19:19:12 -0400

    Display all clinics in index page

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,9 +1,9 @@
 class ClinicsController < ApplicationController
     def index
-        if params[:donor_id]
-            @clinics = Donor.find(params[:clinic_id]).clinics
-        else
-            @clinics = Clinics.all
-        end
+        #if params[:donor_id]
+            #@clinics = Donor.find(params[:clinic_id]).clinics
+        #else
+            @clinics = Clinic.all
+        #end
     end
 end
