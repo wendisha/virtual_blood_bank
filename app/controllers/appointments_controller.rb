d15b3f59a8e60d800e83da10abc6a61e76cd7c51
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-12 20:35:48 -0400

    Changes to new appt

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,14 @@
 class AppointmentsController < ApplicationController
 
     def new
-        @appointment = Appointment.create( :donor_id => params[:donor_id])
+        
+        #@appointment = Appointment.create( :donor_id => params[:donor_id])
         @clinics = Clinic.all
     end
     
+    def create
+        #binding.pry
+        @appointment = Appointment.create( :donor_id => params[:donor_id])
+    end
 end
+
