22d9c72eada5f0798567d0116576f6f052300c70
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-15 22:41:24 -0400

    Add username validator in donor model

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,4 +1,5 @@
 class Donor < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
     has_secure_password
     has_many :appointments
     has_many :clinics, through: :appointments
