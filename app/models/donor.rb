ebd6df255383eae35efc02efb3ffb9b2d05fbbd3
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-21 23:14:03 -0400

    Validate for blood_type

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,5 +1,6 @@
 class Donor < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+    validates :blood_type, presence: true
     has_secure_password
     has_many :appointments
     has_many :clinics, through: :appointments
