32e46e96ecffe881c5a8a174e057fc17e6d53891
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-24 18:31:48 -0400

    Fix indentation and add comments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,69 +3,43 @@ class AppointmentsController < ApplicationController
     before_action :current_donor
     #before_action :redirect_if_not_logged_in!
 
-
     def index
-        @donor = Donor.find(params[:donor_id])
+        @donor = Donor.find(params[:donor_id]) 
     end
 
+    #Identify if nested resource is under clinic or donor, get appropiate parameters
     def new 
-        #binding.pry
         if params[:clinic_id]
             @clinic = Clinic.find(params[:clinic_id])
             params[:donor_id] = current_donor.id
             @appointment = Appointment.new( :clinic_id => params[:clinic_id], :donor_id => params[:donor_id])
         else
-        #if @donor  
             @donor = Donor.find(params[:donor_id])
             @appointment = Appointment.new( :donor_id => params[:donor_id])
             @clinics = Clinic.all
-        #else
-            #@clinic = Clinic.find(params[:clinic_id])
-           # @appointment = Appointment.create( :clinic_id => params[:clinic_id])
-            #@donor
         end
     end
     
+    #Identify if nested resource is under clinic or donor, get appropiate parameters to create appt, and redirect accordingly 
     def create
-        #binding.pry
-        #if @donor
-        #if params[:clinic_id]
-     
-            #@appointment = Appointment.new(appointment_params)
-            #@appointment.donor_id = current_donor.id
-            #@appointment.clinic_id = params[:clinic_id]
-            #binding.pry
-            #if @appointment.save
-               #params[:clinic_id] ? redirect_to (clinic_appointment_path(@appointment.clinic_id, @appointment)) : redirect_to (donor_appointment_path(@appointment.donor_id, @appointment))
-               #redirect_to clinic_appointment_path(@appointment.clinic_id, @appointment)
-               #if params[:clinic_id]
-                #redirect_to (clinic_appointment_path(@appointment.clinic_id, @appointment))
-               #else
-                #redirect_to (donor_appointment_path(@appointment.donor_id, @appointment))
-               #end
-            #else
-                #render plain: "There was an error!"
-           #end
-
-            if params[:clinic_id]
-                @appointment = Appointment.new(appointment_params)
-                @appointment.clinic_id = params[:clinic_id]
-                @appointment.donor_id = current_donor.id
-                if @appointment.save
-                    redirect_to clinic_appointment_path(@appointment.clinic_id, @appointment)
-                else
-                    render plain: "There was an error!"
-                end
+        if params[:clinic_id]
+            @appointment = Appointment.new(appointment_params)
+            @appointment.clinic_id = params[:clinic_id]
+            @appointment.donor_id = current_donor.id
+            if @appointment.save
+                redirect_to clinic_appointment_path(@appointment.clinic_id, @appointment)
             else
-                @appointment = Appointment.new(appointment_params)
-                @appointment.donor_id = current_donor.id
-                if @appointment.save
-                    redirect_to (donor_appointment_path(@appointment.donor_id, @appointment))
-                else
-                    render plain: "There was an error!"
-                end
+                render plain: "There was an error!"
             end
-
+        else
+            @appointment = Appointment.new(appointment_params)
+            @appointment.donor_id = current_donor.id
+            if @appointment.save
+                redirect_to (donor_appointment_path(@appointment.donor_id, @appointment))
+            else
+                render plain: "There was an error!"
+            end
+        end
     end
 
     def show
@@ -73,9 +47,9 @@ class AppointmentsController < ApplicationController
     end
 
     private
+
+    # whitelist the permitted params when sending form data to the db
     def appointment_params
         params.require(:appointment).permit(:datetime, :donor_id, :clinic_id)
     end
-
 end
-
