549f9d52999858086000f2e15d7567424f2d2a3e
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-24 11:55:06 -0400

    Created new nested route for clinics/:id/appts/new and show

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,21 +8,37 @@ class AppointmentsController < ApplicationController
         @donor = Donor.find(params[:donor_id])
     end
 
-    def new  
-        @donor = Donor.find(params[:donor_id])
-        @appointment = Appointment.create( :donor_id => params[:donor_id])
-        @clinics = Clinic.all
+    def new 
+        if @donor  
+            @donor = Donor.find(params[:donor_id])
+            @appointment = Appointment.create( :donor_id => params[:donor_id])
+            @clinics = Clinic.all
+        else
+            @clinic = Clinic.find(params[:clinic_id])
+            @appointment = Appointment.create( :clinic_id => params[:clinic_id])
+            #@donor
+        end
     end
     
     def create
-        @appointment = Appointment.new(appointment_params)
-        @appointment.donor_id = params[:donor_id]
-        if @appointment.save
-            redirect_to donor_appointment_path(@appointment.donor_id, @appointment)
+        if @donor
+            @appointment = Appointment.new(appointment_params)
+            @appointment.donor_id = params[:donor_id]
+            if @appointment.save
+                redirect_to donor_appointment_path(@appointment.donor_id, @appointment)
+            else
+                render plain: "There was an error!"
+            end
         else
-            render plain: "There was an error!"
+            @appointment = Appointment.new(appointment_params)
+            @appointment.clinic_id = params[:clinic_id]
+            @appointment.donor_id = current_donor.id
+            if @appointment.save
+                redirect_to clinic_appointment_path(@appointment.clinic_id, @appointment)
+            else
+                render plain: "There was an error!"
+            end
         end
-
     end
 
     def show
