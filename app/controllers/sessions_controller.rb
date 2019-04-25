6a6d6af04a906d7c91e0033fe9879421f6eb2bd1
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-25 13:31:13 -0400

    Add comments and flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        #Check if donor is signing in through Facebook
         if auth
             @donor = Donor.find_by(uid: auth["uid"])
+            #Check if donor has already signed in through Facebook before and create session/redirect to donor's details page
             if @donor 
-                #binding.pry
                 session[:donor_id] = @donor.id
                 redirect_to donor_path(@donor)
             else 
+                #If not, create the donor, set session/redirect to donor's details page
                 @donor = Donor.new_donor_from_auth(auth)
                 if @donor.save
                     session[:donor_id] = @donor.id
@@ -21,13 +23,16 @@ class SessionsController < ApplicationController
                     raise @donor.errors.full_messages.inspect
                 end
             end
+        #If user is not signing in through Facebook, search for existing account
         else 
             @donor = Donor.find_by(username: params[:donor][:username])
             if @donor && @donor.authenticate(params[:donor][:password])
                 session[:donor_id] = @donor.id
-                redirect_to donor_path(@donor), notice: "Welcome back to the Virtual Blook Bank!"
+                flash[:message] = "Welcome back to the Virtual Blook Bank!"
+                redirect_to donor_path(@donor)
             else
-            redirect_to signin_path
+                flash[:message] = "Invalid username or password. Please try again."
+                redirect_to signin_path
             end
         end
     end
