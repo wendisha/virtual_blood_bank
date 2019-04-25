d630045c37ec4cd3c27b28579ca3643a431eedc1
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-25 13:02:41 -0400

    Create helper method new_donor_from_auth

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -6,4 +6,18 @@ class Donor < ApplicationRecord
     has_many :clinics, through: :appointments
 
     @@blood_types = ["A+", "O+", "B+", "AB+", "A-", "O-", "B-", "AB-"]
+
+    #helper method to create donor when loggin in through Facebook for the first time
+    def self.new_donor_from_auth(auth)
+        Donor.new(
+            :name => request.env["omniauth.auth"]["info"]["name"], 
+            :email => request.env["omniauth.auth"]["info"]["email"], 
+            :password_digest => '45ladsog', 
+            :image => request.env["omniauth.auth"]["info"]["image"], 
+            :username => "#{oauth_name.downcase.split.join('_')}_#{Random.new.rand(99999)}",
+            :blood_type => "Non-Assigned",
+            :uid => request.env["omniauth.auth"]["uid"],
+            :provider => request.env["omniauth.auth"]["provider"]
+        )
+    end
 end
