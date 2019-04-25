9c5b48585f254eaffbb4b7c513801ba0c8d89981
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-25 16:48:48 -0400

    Create new action for donors_clinics and changed clinics

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,14 +1,15 @@
 class ClinicsController < ApplicationController
     def index
-        #create current_donor and get its state from it
+        @clinics = Clinic.all
+        @donor = current_donor.id
+    end
+
+    def donors_clinics
         if current_donor
             @clinics = Clinic.clinics_near_me(current_donor)
-        else
-            #binding.pry
-            @clinics = Clinic.all
         end
         @donor = current_donor.id
-      end
+    end
 
     def show
         @clinic = Clinic.find_by_id(params[:id])
