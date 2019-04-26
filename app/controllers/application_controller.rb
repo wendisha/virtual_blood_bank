54ba66ce883dd24ef21f8120c320d3ddfad97403
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-25 22:36:48 -0400

    Create authenticate_donor? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
     helper_method :current_donor
     helper_method :logged_in?
+    helper_method :authenticate_donor?
     #before_action :current_donor
     before_action :require_logged_in, except: [:new, :create, :home, :index]
 
     def logged_in?
         !!current_donor
     end
+
+    def authenticate_donor?(donor)
+        current_donor.id == donor.id 
+
+    end
+
     #private method is internal to the implementation of a class, and it can only be called by other instance methods of the class or its subclasses. 
     #Private methods are implicitly invoked on self, and may not be explicitly invoked on an object.
     private
@@ -20,3 +27,4 @@ class ApplicationController < ActionController::Base
     end
 
 end
+
