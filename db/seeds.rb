4a058ace0ae0e39f18a8715f933fb3d866f0f531
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-25 23:31:08 -0400

    Seed states table and add 20 more clinics

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Clinic.destroy_all
 
-10.times do |index|
+30.times do |index|
   Clinic.create!(name: Faker::Lorem.sentence(3),
                         state: Faker::Address.state_abbr)
 end
@@ -25,3 +25,57 @@ blood_types.each do |blood_type|
   BloodType.create(blood_type)
 end
 
+states = [{state: "AL"},
+         {state: "AK"},
+         {state: "AZ"},
+         {state: "AR"},
+         {state: "CA"},
+         {state: "CO"},
+         {state: "CT"},
+         {state: "DE"},
+         {state: "FL"},
+         {state: "GA"},
+         {state: "HI"},
+         {state: "ID"},
+         {state: "IL"},
+         {state: "IN"},
+         {state: "IA"},
+         {state: "KS"},
+         {state: "KY"},
+         {state: "LA"},
+         {state: "ME"},
+         {state: "MD"},
+         {state: "MA"},
+         {state: "MI"},
+         {state: "MN"},
+         {state: "MS"},
+         {state: "MO"},
+         {state: "MT"},
+         {state: "NE"},
+         {state: "NV"},
+         {state: "NH"},
+         {state: "NJ"},
+         {state: "NM"},
+         {state: "NY"},
+         {state: "NC"},
+         {state: "ND"},
+         {state: "OH"},
+         {state: "OK"},
+         {state: "OR"},
+         {state: "PA"},
+         {state: "RI"},
+         {state: "SC"},
+         {state: "SD"},
+         {state: "TN"},
+         {state: "TX"},
+         {state: "UT"},
+         {state: "VT"},
+         {state: "VA"},
+         {state: "WA"},
+         {state: "WV"},
+         {state: "WI"},
+         {state: "WY"}]
+
+states.each do |state|
+  State.create(state)
+end
