4077ed218d5354302bd31a7f81b51b8ac06f252e
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-27 11:29:19 -0400

    Fix create action

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,42 +1,39 @@
 class DonorsController < ApplicationController
+    #Make sure user is logged in before all actions, except when creating a new user
     before_action :require_logged_in, except: [:new, :create]
 
-    #create another helper and redirect if not verified donor
     def new
         @donor = Donor.new
-        @blood_types = ["A+", "O+", "B+", "AB+", "A-", "O-", "B-", "AB-"]
     end
 
     def create
-        @donor = Donor.new(donor_params)
-        if @donor.save
-            #log user in
-            session[:donor_id] = @donor.id
-            flash[:message] = "Successfully Signed Up!"
-            redirect_to donor_path(@donor)
-        else
-            #because of rendering, not redirecting
-            #remember instance variables can only persist for one request
-            render :new
-        end
-    end
-
-    def self.create_with_omniauth(auth)
-        donor = find_or_create_by(uid: auth[‘uid’], provider: auth[‘provider’])
-        donor.email = “#{auth[‘uid’]}@#{auth[‘provider’]}.com”
-        donor.password = auth[‘uid’]
-        donor.name = auth[‘info’][‘name’]
-        if Donor.exists?(donor)
-            donor
-        else
-            donor.save!
-            donor
+        if auth
+            @donor = Donor.find_by(uid: auth["uid"])
+            if @donor 
+                session[:donor_id] = @donor.id
+                redirect_to donor_path(@donor)
+            else 
+                @donor.new_donor_from_auth(auth)
+                if @donor.save
+                    session[:donor_id] = @donor.id
+                    redirect_to donor_path(@donor)
+                else
+                    raise @donor.errors.full_messages.inspect
+                end
+            end
+        else 
+            @donor = Donor.find_by(username: params[:donor][:username])
+            if @donor && @donor.authenticate(params[:donor][:password])
+                   session[:donor_id] = @donor.id
+                redirect_to donor_path(@donor), notice: "Welcome back to the Virtual Blook Bank!"
+            else
+                 redirect_to signin_path
+            end
         end
     end
 
     def show
         @donor = Donor.find_by_id(params[:id])
-        #@message = params[:message]
     end
  
     # whitelist the permitted params when sending form data to the db
@@ -44,5 +41,4 @@ class DonorsController < ApplicationController
     def donor_params
         params.require(:donor).permit(:username, :password, :blood_type, :age, :state)
     end
-
 end
