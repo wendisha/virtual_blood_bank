5691948b053bb3012807f1c8058697906c6369fc
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-27 23:22:11 -0400

    Move auth helper to app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_donor
     helper_method :logged_in?
     helper_method :authenticate_donor?
-    #before_action :current_donor
+    helper_method :auth
     before_action :require_logged_in, except: [:new, :create, :home, :index]
 
     def logged_in?
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
     def authenticate_donor?(donor)
         current_donor.id == donor.id 
-
     end
 
     #private method is internal to the implementation of a class, and it can only be called by other instance methods of the class or its subclasses. 
@@ -26,5 +25,7 @@ class ApplicationController < ActionController::Base
         @current_donor ||= Donor.find(session[:donor_id]) if session[:donor_id]
     end
 
+    def auth
+        request.env['omniauth.auth']
+    end
 end
-
