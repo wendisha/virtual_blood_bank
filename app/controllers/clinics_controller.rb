02832022333a21339efa463669869e76650fa951
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-28 00:08:34 -0400

    Comment code

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,9 +1,12 @@
 class ClinicsController < ApplicationController
+
+    #List all existing clinics
     def index
         @clinics = Clinic.all
         @donor = current_donor.id
     end
 
+    #List all existing clinics in the same state as the donor
     def donors_clinics
         if current_donor
             @clinics = Clinic.clinics_near_me(current_donor)
@@ -11,6 +14,7 @@ class ClinicsController < ApplicationController
         @donor = current_donor.id
     end
 
+    #Show details of an specific clinic
     def show
         @clinic = Clinic.find_by_id(params[:id])
         @donor = current_donor.id
