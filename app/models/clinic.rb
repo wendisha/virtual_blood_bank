ffcf60efeebbf9c1322b7379f2aa25e0166cae5f
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-28 00:15:10 -0400

    Comment code, delete unnecessary lines

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,9 +1,7 @@
 class Clinic < ApplicationRecord
     has_many :appointments
     has_many :donors, through: :appointments
-    #scope :clinics_near_me, -> { where("state = ?", @current_donor.state)}
-    #scope :clinics_near_me, -> { where(state: @current_donor.state) }
+    #Scope method to find clinics in the same state as the donor
     scope :clinics_near_me, -> (current_donor){ where(state: current_donor.state) }
-
 end
 
