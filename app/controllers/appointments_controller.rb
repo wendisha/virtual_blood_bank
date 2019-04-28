7086b4a70380a68cd314d97dcac13bea9349fe9b
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-27 23:32:10 -0400

    Delete commented out code

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,24 +1,20 @@
 class AppointmentsController < ApplicationController
     before_action :require_logged_in
     before_action :current_donor
-    #before_action :authenticate_donor? 
-    #before_action :redirect_if_not_logged_in!
 
     def index
         @donor = Donor.find(params[:donor_id]) 
     end
 
     #Identify if nested resource is under clinic or donor, get appropiate parameters
-    def new 
-        #@donor = Donor.find(params[:donor_id])
-        #redirect_to root_path if !authenticate_donor?(@donor)
+    def new
         if params[:clinic_id]
             @clinic = Clinic.find(params[:clinic_id])
             params[:donor_id] = current_donor.id
             @appointment = Appointment.new( :clinic_id => params[:clinic_id], :donor_id => params[:donor_id])
         else
             @donor = Donor.find(params[:donor_id])
-            redirect_to root_path if !authenticate_donor?(@donor)
+            redirect_to root_path if !authenticate_donor?(@donor) #handle new issue!!
             @appointment = Appointment.new( :donor_id => params[:donor_id])
             @clinics = Clinic.all
         end
@@ -38,7 +34,7 @@ class AppointmentsController < ApplicationController
         else
             @appointment = Appointment.new(appointment_params)
             @appointment.donor_id = current_donor.id
-            if @appointment.save
+            if @appointment.save 
                 redirect_to (donor_appointment_path(@appointment.donor_id, @appointment))
             else
                 render plain: "There was an error!"
