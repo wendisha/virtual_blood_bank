720e57d0d9ab344d32c374638fc4fa8d8eaccf9b
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-29 14:42:22 -0400

    Add validations for date and time

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
     belongs_to :donor 
     belongs_to :clinic
+    validates :clinic_id, presence: true
+    validates :date, presence: true
+    validates :time, presence: true
 end
