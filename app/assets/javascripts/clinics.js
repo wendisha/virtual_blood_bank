c31ae6ac0d6e1da920b77317d0424f53dbb0edfe
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-07-05 23:35:16 -0400

    Create listenForClick function on clinics.js for testing!

--- app/assets/javascripts/clinics.js
+++ app/assets/javascripts/clinics.js
@@ -1,3 +1,11 @@
+//Document ready function to hijack the click event on the See All Clinics link on the donor's show page
 $(() => {
     listenForClick();
 });
+
+const listenForClick = () => {
+    $('.all_clinics').on('click', (e) => {
+        e.preventDefault();
+        console.log('tessssssssssst');
+    });
+}
