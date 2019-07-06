8024c08bce49d4bb19a5e5062ba48cfb85e37c9d
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-07-05 23:38:45 -0400

    Add comments to doc ready and listenForClick funtions

--- app/assets/javascripts/clinics.js
+++ app/assets/javascripts/clinics.js
@@ -1,10 +1,14 @@
 //Document ready function to hijack the click event on the See All Clinics link on the donor's show page
+//Arrow function!
 $(() => {
     listenForClick();
 });
 
 const listenForClick = () => {
+    //The $ is a shortcut for jQuery, and provides an interface to the library.
+    //Listen for click on link with all_clinics class, and use callback function that takes an e (event) parameter
     $('.all_clinics').on('click', (e) => {
+        //Prevent default behavior
         e.preventDefault();
         console.log('tessssssssssst');
     });
