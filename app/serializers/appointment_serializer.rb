7b947ee4d506cf82f6d2f32243411b9bff1557c6
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-07-05 23:13:53 -0400

    Create clinics.js file

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,6 +1,6 @@
 class AppointmentSerializer < ActiveModel::Serializer
   #Pick and choose what we want to render as json
-  attributes :id, :donor_id, :clinic_id
+  attributes :id, :donor_id, :clinic_id, :date, :time
 
   #add associations????
   belongs_to :donor
