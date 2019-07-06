950224efb998a053455c87e438e3a275bcbfee30
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-07-05 23:05:31 -0400

    Add belongs_to donor association to appt serializer

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,7 +1,7 @@
 class AppointmentSerializer < ActiveModel::Serializer
   #Pick and choose what we want to render as json
-  attributes :id, :donor_id, :clinic_id, :date, :time
+  attributes :id, :donor_id, :clinic_id
 
   #add associations????
-  #belongs_to :donor
+  belongs_to :donor
 end
